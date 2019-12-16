df14eee5a768fee854627424bbc541e0fe932b4c
Antonio Garzon iniesta <agarzon-@c2r19s4.42madrid.com>
2019-12-16 15:58:04 +0100

    Pruebas

--- main_printf.c
+++ main_printf.c
@@ -5,14 +5,14 @@ void	ft_putnbr_base(int nbr, char *base);
 
 int main ()
 {
-	unsigned int x = -54564;
+	unsigned int x = -5243;
 	char c = 'a';
 	char g[] = "que pasa";
 	char *p = &c;
 
 	ft_printf("MI PRINTF: INT = %d, CHAR = %c, PORCENTAJE %%, STRING = %s, HEXAM = %X, HEXAm = %x, DECIMAL = %i\n", x, c, g, x, x, x);
 	printf("PRINTF: INT = %d, CHAR = %c, PORCENTAJE %%, STRING = %s, HEXAM = %X, HEXAm = %x, DECIMAL = %i\n", x, c, g, x, x, x);
-	printf("PRUEBAS DE FLAGS DMEMORY = %p, u = %u\n", p, x);
+	printf("PRUEBAS DE FLAGS DMEMORY = %p, u = %-10shola", p, g);
 	
 	return (0);
 }
