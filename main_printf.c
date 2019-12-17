0cd5b5da0b17ca4d5ea9b3949d0ca5ae93a28f72
Antonio Garzon iniesta <agarzon-@c3r14s5.42madrid.com>
2019-12-17 11:58:10 +0100

    Main

--- main_printf.c
+++ main_printf.c
@@ -1,18 +1,16 @@
 #include "lib_ft_printf.h"
 #include <stdio.h>
 
-void	ft_putnbr_base(int nbr, char *base);
-
 int main ()
 {
-	int x = 5.45;
+	int x = 5;
 	char c = 'a';
 	char g[] = "que pasa";
 	char *p = &c;
 
 	ft_printf("MI PRINTF: INT = %d, CHAR = %c, PORCENTAJE %%, STRING = %s, HEXAM = %X, HEXAm = %x, DECIMAL = %i\n", x, c, g, x, x, x);
 	printf("PRINTF: INT = %d, CHAR = %c, PORCENTAJE %%, STRING = %s, HEXAM = %X, HEXAm = %x, DECIMAL = %i\n", x, c, g, x, x, x);
-	printf("PRUEBAS DE FLAGS DMEMORY = %p, u = %05.1fdhola", p, x);
+	printf("PRUEBAS DE FLAGS DMEMORY = %p, u = %d", p, x);
 	
 	return (0);
 }
