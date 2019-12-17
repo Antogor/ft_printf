777e93fb7569c31c62b37ed2c28d94f7f3e4396f
Antonio Garzon iniesta <agarzon-@c3r14s5.42madrid.com>
2019-12-17 12:02:59 +0100

    flags

--- main_printf.c
+++ main_printf.c
@@ -3,14 +3,14 @@
 
 int main ()
 {
-	int x = 5;
+	int x = -534343;
 	char c = 'a';
 	char g[] = "que pasa";
 	char *p = &c;
 
-	ft_printf("MI PRINTF: INT = %d, CHAR = %c, PORCENTAJE %%, STRING = %s, HEXAM = %X, HEXAm = %x, DECIMAL = %i\n", x, c, g, x, x, x);
-	printf("PRINTF: INT = %d, CHAR = %c, PORCENTAJE %%, STRING = %s, HEXAM = %X, HEXAm = %x, DECIMAL = %i\n", x, c, g, x, x, x);
-	printf("PRUEBAS DE FLAGS DMEMORY = %p, u = %d", p, x);
+	ft_printf("MI PRINTF: INT = %d, CHAR = %c, PORCENTAJE %%, STRING = %s, HEXAM = %X, HEXAm = %x, DECIMAL = %i, U = %u\n", x, c, g, x, x, x, x);
+	printf("PRINTF: INT = %d, CHAR = %c, PORCENTAJE %%, STRING = %s, HEXAM = %X, HEXAm = %x, DECIMAL = %i, U = %u\n", x, c, g, x, x, x, x);
+	printf("PRUEBAS DE FLAGS DMEMORY = %p", p);
 	
 	return (0);
 }
