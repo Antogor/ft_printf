357663af47d3525b644152cbb1b8c7ce5765d283
Antonio Garzon iniesta <agarzon-@c3r11s4.42madrid.com>
2020-01-08 12:57:45 +0100

    Main

--- main_printf.c
+++ main_printf.c
@@ -12,18 +12,5 @@ int main ()
 	f = ft_printf("%u\n", x);
 	j = printf("%  d\n", x);
 	printf("MIO: %d, ORIGINAL: %d\n", f,j);
-/*	int u = 20;
-	char c = 'a';
-	char g[] = "que pasa";
-	char *p = &c;
-	int o;
-*/
-	
-/*
-	ft_printf("MI PRINTF: INT = %09dprueba, CHAR = %-3cprueba, PORCENTAJE %%, STRING = %-10.3shola, HEXAM = %X, HEXAm = %x, DECIMAL = %i\n", x, c, g, x, x, x);
-	o = printf("PRINTF: INT = %d, CHAR = %-3cprueba, PORCENTAJE %%, STRING = %s, HEXAM = %X, HEXAm = %x, DECIMAL = %i, U = %u\n", x, c, g, x, x, x, u);
-	o = printf("%d", x);
-	printf("%d\n", o);
-*/
 	return (0);
 }
