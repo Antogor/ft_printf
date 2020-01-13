069b6519ff07dc20b1783369a30101179b064693
Antonio Garzon iniesta <agarzon-@c3r13s4.42madrid.com>
2020-01-13 10:56:43 +0100

    d e i final

--- ft_conversion.c
+++ ft_conversion.c
@@ -6,7 +6,7 @@
 /*   By: agarzon- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/08 10:41:57 by agarzon-          #+#    #+#             */
-/*   Updated: 2020/01/10 11:20:04 by agarzon-         ###   ########.fr       */
+/*   Updated: 2020/01/13 10:54:15 by agarzon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,18 +32,18 @@ void	ft_conversion(t_printf *tab)
 {
 	ft_asig(tab);
 	if (tab->type == 'd' || tab->type == 'i')
-		tab->d == 0 && tab->precision == 0 ? display_0(tab) :
-		display_d(tab->d, tab);
+		tab->d == 0 && tab->precision == 0 && tab->punt == '.' ?
+		display_0(tab) : display_d(tab->d, tab);
 	else if (tab->type == 'c')
 		display_c(tab->c, tab);
 	else if (tab->type == 's')
 		display_s(tab->s, tab);
 	else if (tab->type == 'u')
-		tab->u == 0 && tab->precision == 0 ? display_0(tab) :
-		display_u(tab->u, tab);
+		tab->u == 0 && tab->precision == 0 && tab->punt == '.' ?
+		display_0(tab) : display_u(tab->u, tab);
 	else if (tab->type == 'x' || tab->type == 'X')
-		tab->hexa == 0 && tab->precision == 0 ? display_0(tab) :
-		display_hexa(tab->hexa, tab);
+		tab->hexa == 0 && tab->precision == 0 && tab->punt == '.' ?
+		display_0(tab) : display_hexa(tab->hexa, tab);
 	else if (tab->type == 'p')
 		display_p(tab->p, tab);
 }
--- main_printf.c
+++ main_printf.c
@@ -3,14 +3,14 @@
 
 int main ()
 {
-	int x = 5;
+	int x = 0;
 	int g = -3;
 	char c = 'a';
 	char *s = "hola";
 	int f;
 	int j;
-	f = ft_printf("%.dhola\n", x);
-	j = printf("%.dhola\n", x);
+	f = ft_printf("%dhola\n", x);
+	j = printf("%dhola\n", x);
 	printf("MIO: %d, ORIGINAL: %d\n", f,j);
 	return (0);
 }
